797e22c36f63865447585be31db5dc325172b3ff
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2023-01-17 09:37:56 +0700

    fix preview problems

--- app/src/main/java/github/luthfipun/chatroom/screen/HomeScreen.kt
+++ app/src/main/java/github/luthfipun/chatroom/screen/HomeScreen.kt
@@ -2,6 +2,7 @@ package github.luthfipun.chatroom.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -9,11 +10,13 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showBackground = true)
 @Composable
-fun HomeScreen(){
+fun HomeScreen(
+    modifier: Modifier = Modifier
+){
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-
+        Text(text = "hello world")
     }
 }
--- app/src/main/java/github/luthfipun/chatroom/screen/MainActivity.kt
+++ app/src/main/java/github/luthfipun/chatroom/screen/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import github.luthfipun.chatroom.screen.ui.theme.ChatRoomTheme
 
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Text(text = "hello world")
                 }
             }
         }
