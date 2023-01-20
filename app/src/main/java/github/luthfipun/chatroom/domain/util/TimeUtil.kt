c104aa2ed5eb1357a71373e37f766b188b3f9866
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2023-01-20 14:20:19 +0700

    setup time utils

---
+++ app/src/main/java/github/luthfipun/chatroom/domain/util/TimeUtil.kt
@@ -0,0 +1,14 @@
+package github.luthfipun.chatroom.domain.util
+
+import android.content.Context
+import android.text.format.DateUtils
+
+fun Context.formatMessageTime(time: Long): String {
+    return DateUtils.formatDateTime(this, time, DateUtils.FORMAT_SHOW_TIME)
+}
+
+fun Context.formatMessageInfo(time: Long): String {
+    return if (DateUtils.isToday(time)){
+        DateUtils.getRelativeTimeSpanString(time).toString()
+    }else DateUtils.formatDateTime(this, time, DateUtils.FORMAT_SHOW_WEEKDAY)
+}
